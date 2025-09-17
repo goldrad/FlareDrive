f4d14ef23fb6e89127d829c8cc069d418f0a14ef
goldrad <hoseinrad1500@gmail.com>
2025-09-17 13:45:09 +0330

    Update propfind.ts

--- functions/webdav/propfind.ts
+++ functions/webdav/propfind.ts
@@ -1,104 +1,108 @@
 import {
-  listAll,
-  RequestHandlerParams,
-  ROOT_OBJECT,
-  WEBDAV_ENDPOINT,
+  listAll,
+  RequestHandlerParams,
+  ROOT_OBJECT,
+  WEBDAV_ENDPOINT,
 } from "./utils";
 
 type DavProperties = {
-  creationdate: string | undefined;
-  displayname: string | undefined;
-  getcontentlanguage: string | undefined;
-  getcontentlength: string | undefined;
-  getcontenttype: string | undefined;
-  getetag: string | undefined;
-  getlastmodified: string | undefined;
-  resourcetype: string;
-  "fd:thumbnail": string | undefined;
+  creationdate: string | undefined;
+  displayname: string | undefined;
+  getcontentlanguage: string | undefined;
+  getcontentlength: string | undefined;
+  getcontenttype: string | undefined;
+  getetag: string | undefined;
+  getlastmodified: string | undefined;
+  resourctype: string;
+  "fd:thumbnail": string | undefined;
 };
 
 function fromR2Object(object: R2Object | typeof ROOT_OBJECT): DavProperties {
-  return {
-    creationdate: object.uploaded.toUTCString(),
-    displayname: object.httpMetadata?.contentDisposition,
-    getcontentlanguage: object.httpMetadata?.contentLanguage,
-    getcontentlength: object.size.toString(),
-    getcontenttype: object.httpMetadata?.contentType,
-    getetag: object.etag,
-    getlastmodified: object.uploaded.toUTCString(),
-    resourcetype:
-      object.httpMetadata?.contentType === "application/x-directory"
-        ? "<collection />"
-        : "",
-    "fd:thumbnail": object.customMetadata?.thumbnail,
-  };
+  return {
+    creationdate: object.uploaded.toUTCString(),
+    displayname: object.httpMetadata?.contentDisposition,
+    getcontentlanguage: object.httpMetadata?.contentLanguage,
+    getcontentlength: object.size.toString(),
+    getcontenttype: object.httpMetadata?.contentType,
+    getetag: object.etag,
+    getlastmodified: object.uploaded.toUTCString(),
+    resourcetype:
+      object.httpMetadata?.contentType === "application/x-directory"
+        ? "<collection />"
+        : "",
+    "fd:thumbnail": object.customMetadata?.thumbnail,
+  };
 }
 
 async function findChildren({
-  bucket,
-  path,
-  depth,
+  bucket,
+  path,
+  depth,
 }: {
-  bucket: R2Bucket;
-  path: string;
-  depth: string;
+  bucket: R2Bucket;
+  path: string;
+  depth: string;
 }) {
-  if (!["1", "infinity"].includes(depth)) return [];
+  if (!["1", "infinity"].includes(depth)) return [];
 
-  const objects: Array<R2Object> = [];
+  const objects: Array<R2Object> = [];
 
-  const prefix = path === "" ? path : `${path}/`;
-  for await (const object of listAll(bucket, prefix, depth === "infinity")) {
-    objects.push(object);
-  }
+  const prefix = path === "" ? path : `${path}/`;
+  for await (const object of listAll(bucket, prefix, depth === "infinity")) {
+    objects.push(object);
+  }
 
-  return objects;
+  return objects;
 }
 
 export async function handleRequestPropfind({
-  bucket,
-  path,
-  request,
+  bucket,
+  path,
+  request,
 }: RequestHandlerParams) {
-  const responseTemplate = `<?xml version="1.0" encoding="utf-8" ?>
+  const responseTemplate = `<?xml version="1.0" encoding="utf-8" ?>
 <multistatus xmlns="DAV:" xmlns:fd="flaredrive">
 {{items}}
 </multistatus>`;
 
-  const rootObject = path === "" ? ROOT_OBJECT : await bucket.head(path);
-  if (!rootObject) return new Response("Not found", { status: 404 });
-  const isDirectory =
-    rootObject === ROOT_OBJECT ||
-    rootObject.httpMetadata?.contentType === "application/x-directory";
-  const depth = request.headers.get("Depth") ?? "infinity";
+  const rootObject = path === "" ? ROOT_OBJECT : await bucket.head(path);
+  if (!rootObject) return new Response("Not found", { status: 404 });
+  const isDirectory =
+    rootObject === ROOT_OBJECT ||
+    rootObject.httpMetadata?.contentType === "application/x-directory";
+  const depth = request.headers.get("Depth") ?? "infinity";
 
-  const children = !isDirectory
-    ? []
-    : await findChildren({
-        bucket,
-        path,
-        depth,
-      });
+  const children = !isDirectory
+    ? []
+    : await findChildren({
+        bucket,
+        path,
+        depth,
+      });
 
-  const items = [rootObject, ...children].map((child) => {
-    const properties = fromR2Object(child);
-    return `
-  <response>
-    <href>${encodeURI(`${WEBDAV_ENDPOINT}${child.key}`)}</href>
-    <propstat>
-      <prop>
-        ${Object.entries(properties)
-          .filter(([_, value]) => value !== undefined)
-          .map(([key, value]) => `<${key}>${value}</${key}>`)
-          .join("\n")}
-      </prop>
-      <status>HTTP/1.1 200 OK</status>
-    </propstat>
-  </response>`;
-  });
+  const items = [rootObject, ...children].map((child) => {
+    const properties = fromR2Object(child);
+    return `
+  <response>
+    <href>${encodeURI(`${WEBDAV_ENDPOINT}${child.key}`)}</href>
+    <propstat>
+      <prop>
+        ${Object.entries(properties)
+          .filter(([_, value]) => value !== undefined)
+          .map(([key, value]) => `<${key}>${value}</${key}>`)
+          .join("\n")}
+      </prop>
+      <status>HTTP/1.1 200 OK</status>
+    </propstat>
+  </response>`;
+  });
 
-  return new Response(responseTemplate.replace("{{items}}", items.join("")), {
-    status: 207,
-    headers: { "Content-Type": "application/xml" },
-  });
+  return new Response(responseTemplate.replace("{{items}}", items.join("")), {
+    status: 207,
+    headers: { 
+        "Content-Type": "application/xml",
+        // ▼▼▼ این خط کد اضافه شده تا مشکل کش حل شود ▼▼▼
+        "Cache-Control": "no-store, no-cache, must-revalidate",
+     },
+  });
 }
